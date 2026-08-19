efa76a75170fa82c2b4c4704e412284a9459d99f
Kowiel <michal.patryk.urban@gmail.com>
2026-08-19 23:03:11 +0200

    build: adding mudblazor and configuring it

--- HouseLedger.Client/Program.cs
+++ HouseLedger.Client/Program.cs
@@ -1,3 +1,4 @@
+using MudBlazor.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,6 +12,7 @@ namespace HouseLedger.Client
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
+            builder.Services.AddMudServices();
             builder.Services.AddScoped(sp => new HttpClient 
             { 
                 BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
